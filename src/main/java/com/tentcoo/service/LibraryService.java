744ff5d68615a7ab93fedf223197d582e0697d3d
ZhangYongWei1955 <785712604@qq.com>
2018-11-28 21:18:57 +0800

    add controller

---
+++ src/main/java/com/tentcoo/service/LibraryService.java
@@ -0,0 +1,14 @@
+package com.tentcoo.service;
+
+import com.tentcoo.library.pojo.BookDetail;
+import com.tentcoo.pojo.QueryList;
+
+public interface LibraryService {
+	
+	public QueryList queryByWord(String word,int page);
+	
+	public QueryList queryCategory(String url);
+	
+	public BookDetail getBookDetail(String url);
+	
+}
